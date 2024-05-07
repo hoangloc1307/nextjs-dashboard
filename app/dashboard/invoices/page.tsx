91e4375f13ff042d731b7321393ec1bdb9ad575a
Lộc Trần <hoangloc1307@gmail.com>
2024-05-07 21:57:49 +0700

    finished course

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,13 +6,18 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
 type Props = {
   searchParams?: {
     query?: string;
     page?: string;
   };
-}
+};
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({ searchParams }: Props) {
   const query = searchParams?.query || '';
@@ -29,7 +34,7 @@ export default async function Page({ searchParams }: Props) {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
